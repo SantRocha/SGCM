5e6de5c9af1673af3ef10bc8eeed1eb0a5a0ded5
SantRocha <santiagomelo121@gmail.com>
2024-05-20 19:50:38 -0500

    atividade

--- javascript/script.js
+++ javascript/script.js
@@ -1,57 +1,21 @@
-// Identifica o tema selecionado na página
-let selectTema = document.querySelector('select#tema');
-selectTema.addEventListener('change', evento => {
-    // Obtém o valor do tema selecionado
-    let temaselecionado = evento.target.value;
-
-    // Verifica se um tema foi selecionado
-    if (temaselecionado){
-        // Chama a função para mudar o tema e armazena o tema selecionado no armazenamento local
-        mudaTema(temaselecionado);
-        localStorage.setItem('tema', temaselecionado)
-    }
-})
-
-// Define o tema da Página de acordo com o selecionado
-const mudaTema = (temaselecionado) => {
-    // Obtém o link para a folha de estilo do tema selecionado
-    let linkTema = document.querySelector('#linkTema');
-    let url = "../css/" + temaselecionado + '.css';
-    // Define a nova URL para o link da folha de estilo
-    linkTema.href = url;
-
-    // Obtém o elemento da imagem do logotipo
-    let imglogo = document.querySelector('#imglogo');
-    // Verifica o tema selecionado e atualiza a imagem do logotipo de acordo
-    if(temaselecionado == 'amarelo' || temaselecionado == 'vermelho'){
-        imglogo.src = "../imagens/logo_branco(64).png"
-    }
-    else{
-        imglogo.src = "../imagens/logo_azul(64).png"
-    }
-}
-
-// Garante que o tema seja o mesmo com a atualização da página
-let tema = localStorage.getItem('tema');
-if (tema) {
-    mudaTema(tema)
-}
-
-// Cria a funcionalidade dos botões adicionar e cancelar da página que controlam o formulário.
+// Cria a funcionalidade dos botões adicionar e cancelar da página que controlam o formulário. funcao adicionada para esconder
 let botaoAdicionar = document.querySelector('.adicionar');
 let botaoCancelar = document.querySelector('.cancelar');
 let form = document.querySelector('form');
+let botaoEsconder2 = document.querySelector('.esconder2');
 
 // Ao clicar no botão Adicionar, remove a classe que esconde o formulário.
 botaoAdicionar.addEventListener('click', (event) => {
     form.classList.remove('inativo');
+    botaoEsconder2.classList.add('esconder');
     event.preventDefault();
 });
 
-// Ao clicar no botão Cancelar, adiciona a classe que esconde o formulário e o reseta.
+// Ao clicar no botão Cancelar, adiciona a classe que esconde o formulário e o reseta. foi adicionado um variavel botaoEsconder2.classList.remove('.esconder');
 botaoCancelar.addEventListener('click', (event) => {
     form.classList.add('inativo');
     form.reset();
+    botaoEsconder2.classList.remove('esconder');
     event.preventDefault();
 });
 
@@ -59,6 +23,7 @@ botaoCancelar.addEventListener('click', (event) => {
 let idTabela = document.querySelector('table');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+    botaoEsconder2.classList.remove('esconder');
     // Cria um objeto com os dados do profissional
     let profissional = {
         id: idTabela.tBodies[0].rows.length + 1,
@@ -121,7 +86,9 @@ const inserirProfissional = (item) => {
     let botaoExcluir = linha.querySelector('.excluir');
     botaoExcluir.addEventListener('click', () => {
         linha.remove();
+        rodapeTabela();// chamaa  funçao para reduzir
     })
+    rodapeTabela();//chama a funçao para adicionar 
 };
 
 // Carrega as Informações do JSON na tabela
@@ -143,3 +110,10 @@ const carregaTabela = () => {
 }
 // Chama a função para carregar a tabela quando a página é carregada
 carregaTabela();
+
+// Atualiza o rodapé para a quantidade de linha da tabela.
+const rodapeTabela = () => {
+    const totalSpan = document.getElementById('total');
+    const totalRegistros = idTabela.tBodies[0].rows.length;
+    totalSpan.textContent = totalRegistros;
+};
