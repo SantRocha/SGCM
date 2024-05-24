9acc49d4dbb8f97366d611613684d47d3ca3714a
santrocha <santiagomelo121@gmail.com>
2024-05-23 20:25:47 -0500

    java

---
+++ java-back-sgcm/sgcm/src/main/java/App.java
@@ -0,0 +1,35 @@
+import br.ufac.sgcm.dao.ConexaoDB;
+import br.ufac.sgcm.model.Especialidade;
+import br.ufac.sgcm.model.Profissional;
+import br.ufac.sgcm.model.Unidade;
+
+public class App {
+    public static void main(String[] args) {
+        Profissional p1 = new Profissional();
+        p1.setId(1L);
+        p1.setNome("Santiago");
+        p1.setEmial("santiagomelo121@gmail.com");
+        p1.setTelefone("(68) 99225-2856");
+        p1.setRegistro("CRM/AC-007");
+
+        Unidade u1 = new Unidade();
+        u1.setId(1L);
+        u1.setNome("Pronto Atendimento");
+        u1.setEndereco("Av. Rocha Viana - 204");
+
+        Especialidade e1 = new Especialidade();
+        e1.setId(1L);
+        e1.setNome("Pediatria");
+
+        p1.setUnidade(u1);
+        p1.setEspecialidade(e1);
+
+        System.out.println(p1.getNome());
+        System.out.println(p1.getUnidade().getNome());
+        System.out.println(p1.getEspecialidade().getNome());
+
+        ConexaoDB conexao = new ConexaoDB();
+        conexao.getConexao();
+
+    }
+}
---
+++ java-back-sgcm/sgcm/src/main/java/br/ufac/sgcm/model/Profissional.java
@@ -0,0 +1,60 @@
+package br.ufac.sgcm.model;
+
+import java.io.Serializable;
+
+public class Profissional implements Serializable {
+    private Long id;
+    private String nome;
+    private String registro;
+    private String emial;
+    private String telefone;
+    private Unidade unidade;
+    private Especialidade especialidade;
+
+    public Profissional() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRegistro() {
+        return registro;
+    }
+    public void setRegistro(String registro) {
+        this.registro = registro;
+    }
+    public String getEmial() {
+        return emial;
+    }
+    public void setEmial(String emial) {
+        this.emial = emial;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public Unidade getUnidade() {
+        return unidade;
+    }
+    public void setUnidade(Unidade unidade) {
+        this.unidade = unidade;
+    }
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+}
